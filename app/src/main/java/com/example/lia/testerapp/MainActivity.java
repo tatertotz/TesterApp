b0250f08229159bd73dfe6221f739886931f5d18
tatertotz <Student@Avalons-Mac-mini.local>
2017-02-13 15:29:46 -0600

    I initialized an int variable called dog

--- app/src/main/java/com/example/lia/testerapp/MainActivity.java
+++ app/src/main/java/com/example/lia/testerapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     int cat;
+    int dog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

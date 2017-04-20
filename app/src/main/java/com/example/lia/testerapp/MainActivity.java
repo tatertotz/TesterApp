ad0d21c6e178d37f35ef9dbedc70c87119b63c95
Alia Tang <lia@Evans-iMac.local>
2017-04-19 19:50:25 -0500

    I changed a whole bunch of stuff, so now, you type in a text field, press the button, and it takes you to another screen and shows the text you had typed in the text field

--- app/src/main/java/com/example/lia/testerapp/MainActivity.java
+++ app/src/main/java/com/example/lia/testerapp/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.lia.testerapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Spinner;
-import android.widget.TextView;
-
-import java.util.List;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private SushiExpert expert = new SushiExpert();
@@ -18,24 +16,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //Call when the user clicks the button
-    public void onClickFindSushi(View view){
-        //Get a reference to the TextView
-        TextView types = (TextView) findViewById(R.id.types);
-
-        //Get a reference to the Spinner
-        Spinner style = (Spinner) findViewById(R.id.style);
-
-        //Get the selected item in the Spinner
-        String sushiStyle = String.valueOf(style.getSelectedItem());
-
-
-        //Get recommendations from SushiExpert class
-        List<String> typesList = expert.getTypes(sushiStyle);
-        StringBuilder typesFormatted = new StringBuilder();
-        for (String type : typesList){
-            typesFormatted.append(type).append('\n');
-        }
-        //Display the beers
-        types.setText(typesFormatted);
+    public void onSendMessage(View view){
+        EditText messageView = (EditText)findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra("message", messageText);
+        startActivity(intent);
     }
 }

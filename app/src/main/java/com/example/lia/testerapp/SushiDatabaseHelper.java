1ca69c38e2e5b1f50b7db987fc02f037b9d910c0
tatertotz <Student@Avalons-Mac-mini.local>
2017-05-25 10:46:06 -0500

    Still trying to revert stuff.
    This reverts commit 799bb0725fc97a5d70cf82f39d879d3a7d4e1e9d.

--- app/src/main/java/com/example/lia/testerapp/SushiDatabaseHelper.java
+++ app/src/main/java/com/example/lia/testerapp/SushiDatabaseHelper.java
@@ -12,7 +12,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class SushiDatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "sushi";
-    private static final int DB_VERSION = 1;
+    private static final int DB_VERSION = 2;
 
     SushiDatabaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -20,18 +20,27 @@ public class SushiDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE SUSHI ("
-        + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
-        + "NAME TEXT, "
-        + "TYPE TEXT, "
-        + "DESCRIPTION TEXT);");
-
-//        insertSushi(db, "Meow")
+        updateMyDatabase(db, 0, DB_VERSION);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        updateMyDatabase(db, oldVersion, newVersion);
+    }
 
+    public void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion < 1) {
+            db.execSQL("CREATE TABLE SUSHI (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + "NAME TEXT, "
+            + "TYPE TEXT, "
+            + "DESCRIPTION TEXT);");
+            insertSushi(db, "Salmon Roll", "Maki", "Yummy");
+            insertSushi(db, "Salmon Nigiri", "Nigiri", "Tasty");
+            insertSushi(db, "California Roll", "Maki", "Mmmm");
+        }
+        if (oldVersion < 2) {
+            db.execSQL("ALTER TABLE DRINK ADD COLUMN RATING NUMERIC;");
+        }
     }
 
     public static void insertSushi(SQLiteDatabase db, String name, String type, String description) {

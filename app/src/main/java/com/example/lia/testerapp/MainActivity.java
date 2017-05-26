37fda9c7d0f9acea0d82ddd6019c8f8b35588e07
tatertotz <Student@Avalons-Mac-mini.local>
2017-05-26 11:24:08 -0500

    Still fixing stuff

--- app/src/main/java/com/example/lia/testerapp/MainActivity.java
+++ app/src/main/java/com/example/lia/testerapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.lia.testerapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private SushiExpert expert = new SushiExpert();
@@ -16,14 +18,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //Call when the user clicks the button
-    public void onSendMessage(View view){
-        EditText messageView = (EditText)findViewById(R.id.message);
-        String messageText = messageView.getText().toString();
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_TEXT, messageText);
-        String chooserTitle = getString(R.string.chooser);
-        Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
-        startActivity(chosenIntent);
+    public void onClickFindSushi(View view){
+        //Get a reference to the TextView
+        TextView types = (TextView) findViewById(R.id.types);
+
+        //Get a reference to the Spinner
+        Spinner style = (Spinner) findViewById(R.id.style);
+
+        //Get the selected item in the Spinner
+        String sushiStyle = String.valueOf(style.getSelectedItem());
+
+
+        //Get recommendations from SushiExpert class
+        List<String> typesList = expert.getTypes(sushiStyle);
+        StringBuilder typesFormatted = new StringBuilder();
+        for (String type : typesList){
+            typesFormatted.append(type).append('\n');
+        }
+        //Display the beers
+        types.setText(typesFormatted);
     }
 }

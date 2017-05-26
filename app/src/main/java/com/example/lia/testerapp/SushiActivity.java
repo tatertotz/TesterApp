83a8d72418d596b6a74183772e605251aba8b772
tatertotz <Student@Avalons-Mac-mini.local>
2017-05-26 14:28:10 -0500

    Turning the sushiPicker branch into a trial of databases (chap 12) and chap 6

---
+++ app/src/main/java/com/example/lia/testerapp/SushiActivity.java
@@ -0,0 +1,13 @@
+package com.example.lia.testerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SushiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sushi);
+    }
+}
---
+++ app/src/main/java/com/example/lia/testerapp/SushiCategoryActivity.java
@@ -0,0 +1,13 @@
+package com.example.lia.testerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SushiCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sushi_category);
+    }
+}
